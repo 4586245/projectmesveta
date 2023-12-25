bf362f45cb2e2f9fee93d2be0829215de5c75226
student <pn.gerebennikova@gmail.com>
2023-12-25 15:59:31 +0900

    project

--- 1.py
+++ 1.py
@@ -1,5 +1,74 @@
+import pygame
+import os
+import sys
+import random
+
+FPS = 50
+pygame.init()
+screen = pygame.display.set_mode((400, 400))
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+def start_screen(screen):
+    clock = pygame.time.Clock()
+    intro_text = ["Утка в погоне за утятами", "",
+                  "Ищи люгушек для утят, чтобы их прокормить"]
+                  # "Если в правилах несколько строк,",
+                  # "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'),
+                                 (screen.get_width(), screen.get_height()))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
 def main():
-    print('My first git program')
+    running = True
+    pygame.display.flip()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+    pygame.quit()
+
 
 
 if __name__ == '__main__':
